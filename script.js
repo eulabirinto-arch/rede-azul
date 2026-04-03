2054ec450387b392990a6f82b4501fb6ae5449e2
eulabirinto-arch <eulabirinto@gmail.com>
2026-04-03 02:33:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-function addPost() {
-    let text = document.getElementById("postText").value;
-    let posts = document.getElementById("posts");
-
-    if (text === "") return;
-
-    let newPost = document.createElement("div");
-    newPost.className = "post";
-    newPost.innerHTML = "<p>" + text + "</p>";
-
-    posts.prepend(newPost);
-    document.getElementById("postText").value = "";
-}
-
 function acolhimento() {
     alert("Respire fundo 💙 Você não está sozinha.");
+}
+
+function setHumor(h) {
+    document.getElementById("humor").innerText = "Humor: " + h;
 }
