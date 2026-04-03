5125bf97b4dff51514043e7a66ac8eb8ca7e45dd
eulabirinto-arch <eulabirinto@gmail.com>
2026-04-03 02:53:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,3 @@ document.getElementById("fala").innerText = texto;
 let msg = new SpeechSynthesisUtterance(texto);
 speechSynthesis.speak(msg);
 }
-
-function scrollToSection(){
-document.getElementById("sobre").scrollIntoView({
-behavior:"smooth"
-});
-}
