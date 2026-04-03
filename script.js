196b7532362490f79a6d33d644fa9ed8318e3f0d
eulabirinto-arch <eulabirinto@gmail.com>
2026-04-03 02:58:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ document.getElementById("fala").innerText = texto;
 let msg = new SpeechSynthesisUtterance(texto);
 speechSynthesis.speak(msg);
 }
+
+/* SCROLL SUAVE */
+function scrollToSection(){
+document.getElementById("sobre").scrollIntoView({
+behavior:"smooth"
+});
+}
+
+/* ANIMAÇÃO AO ROLAR */
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+
+document.querySelectorAll('.fade').forEach(el => observer.observe(el));
